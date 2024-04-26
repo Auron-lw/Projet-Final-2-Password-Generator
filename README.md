f0c4abfe49a4310f52ea49978378f4fc15a186d8
Julien Miguel <miguel.julien.pro@gmail.com>
2024-04-26 18:28:40 +0200

    Adding README + first try
    Signed-off-by: Julien Miguel <miguel.julien.pro@gmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Projet-Final-2-Password-Generator
+
+Instructions
+
+Créer un générateur de mot de passe en utilisant le terminal, voici à quoi ça va ressembler :
+
+    🔢 Combien de caractères ? (8-36)
+    10
+    🔣 Caractères spéciaux ? (y/n)
+    y
+    🔢 Chiffres ? (y/n)
+    y
+    ⬆️ Majuscules ? (y/n)
+    y
+    Votre mot de passe généré est : GZC@cbwOTb
--- index.js
+++ index.js
@@ -1,1 +1,68 @@
 import { prompt } from "./prompt.js";
+
+const generateRandomNumber = (min, max) => {
+  return Math.random() * (max - min) + min;
+};
+
+const generateSpecialChar = () => {
+  const s = '!"§$%&/()=?\u{20ac}';
+
+  const randomIndex = Math.floor(Math.random() * s.length);
+
+  return s.substring(randomIndex, randomIndex + 1);
+};
+
+const generateUpperCaseChar = () => {
+  const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+  const randomIndex = Math.floor(Math.random() * alphabet.length);
+
+  return alphabet.substring(randomIndex, randomIndex + 1);
+};
+
+const questions = () => {
+  // Range of characters ?
+  const charRange = Number(
+    prompt(
+      `How many characters you want (8-36) ?
+    > `
+    )
+  );
+  // specials characters ?
+  const charSpecial = prompt(
+    `Do you want special characters (y/n) ?
+    > `
+  ).toLowerCase();
+  // includes numbers ?
+  const charNumbers = prompt(
+    `Do you want numbers (y/n) ?
+    > `
+  ).toLowerCase();
+  // includes uppercase ?
+  const charUpper = prompt(
+    `Do you want uppercase letters (y/n) ?
+    > `
+  ).toLowerCase();
+  console.log("----------------------------------------------");
+
+  if (!charRange || !charSpecial || !charNumbers || !charUpper) {
+    questions();
+  }
+
+  if (charRange < 8 || charRange > 36) {
+    questions();
+  }
+
+  if (charSpecial === "y") {
+    generateSpecialChar();
+  }
+
+  if (charNumbers === "y") {
+    generateRandomNumber(1, 9);
+  }
+
+  if (charUpper === "y") {
+    generateUpperCaseChar();
+  }
+};
+questions();
